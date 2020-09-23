4d0a338e78778cf883175438c03b0e8af236c83f
shivaadhikari216 <70180482+shivaadhikari216@users.noreply.github.com>
2020-09-22 22:34:38 -0400

    third commit

---
+++ pythonhw1.py
@@ -0,0 +1,4 @@
+#https://github.com/shivaadhikari216/bcch-python.git
+Farenheit=int(input("Enter the temeprature in degrees Farenheit :"))
+K= ((Farenheit-32) * 5/9 + 273.15)
+print ("Temperature in degree Kelvin is :",K)
